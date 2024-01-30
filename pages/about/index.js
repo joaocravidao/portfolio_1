b8eca0e7ed86aaa02ba524eb1c53b12bce81542d
João Cravidão <joao_kravidao@hotmail.com>
2024-01-30 15:28:38 +0000

    new update

--- pages/about/index.js
+++ pages/about/index.js
@@ -224,7 +224,7 @@ const About = () => {
                   <div>{item.stage}</div>
                   <div className="flex gap-x-4">
                     {/* icons */}
-                    {item.icons?.map((icon, itemIndex) => {
+                    {item.icons?.map((icon) => {
                       return <div className="text-2xl text-white">{icon}</div>;
                     })}
                   </div>
