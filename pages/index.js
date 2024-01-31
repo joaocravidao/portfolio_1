8c4aac4c2234e0325c5a2c225bf23722f94d54b0
João Cravidão <joao_kravidao@hotmail.com>
2024-01-31 11:31:29 +0000

    last update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-// next image
-import Image from "next/image";
-
 // components
 import ParticlesContainer from "../components/ParticlesContainer";
 import ProjectsBtn from "../components/ProjectsBtn";
